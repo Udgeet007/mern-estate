21e765b58aa43c2f8354f37d078d0028a2da06ae
Udgeet <bhatt8udgeet@gmail.com>
2023-11-21 19:27:22 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes , Route } from "react-router-dom"
 import Home from "./pages/Home"
-import About from "./pages/about"
+import About from "./pages/About"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
